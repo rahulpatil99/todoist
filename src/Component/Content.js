906fdf5400ddcd4e1671b51e326c82ca74962ca8
Rahul <rahulpatil989018@gmail.com>
2021-12-05 21:46:00 +0530

    task design

--- src/Component/Content.js
+++ src/Component/Content.js
@@ -1,11 +1,12 @@
 import React, { useState} from 'react'
 import Sidebar from './Sidebar'
 import Task from './Task'
+import './Content.css'
 
 export default function Content() {
   const [selectTab, setSelectTab] = useState("INDEX");
   return (
-    <div>
+    <div className="content">
       <Sidebar selectTab={selectTab} setSelectTab={setSelectTab}/>
       <Task/>
     </div>
--- src/Component/Task.js
+++ src/Component/Task.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import './Task.css'
 
 export default function Task() {
   return (
-    <div></div>
+    <div className="task">
+      <h1>Inbox</h1>
+      <div>
+        <span>+</span>
+        <span>Add Task</span>
+      </div>
+      <div>
+        <input type="text" className="form-control"/>
+        <button className="btn btn-success">Send</button>
+        <button className="btn btn-secondary m-2">Cancel</button>
+      </div>
+    </div>
   )
 }
